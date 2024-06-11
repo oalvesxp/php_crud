eeb4bdcab61e2de8aeaf607796904a551c5d03e6
OsvaldoNeto <osvaldo.neto@signus.ind.br>
2024-06-11 14:42:30 -0300

    adicionando funcao de salvar produto

--- src/Domain/Repository/ProductRepository.php
+++ src/Domain/Repository/ProductRepository.php
@@ -2,10 +2,13 @@
 
 namespace Serenatto\Crud\Domain\Repository;
 
+use Serenatto\Crud\Domain\Model\Product;
+
 interface ProductRepository
 {
     public function itensCafe(): array;
     public function itensAlmoco(): array;
     public function allProducts(): array;
     public function deletar(int $id): void;
+    public function salvar(Product $produto): void;
 }
